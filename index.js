198ec775e56ed784e998e9b671d7606aadb052a5
kaiomagalhaes <me@kaiomagalhaes.com>
2018-11-03 16:35:27 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
+const userModel = require('./models/user')
+const userServices = require('./services/user')
+const stepsServices = require('./services/steps')
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-const port = 3000
+const port = process.env.PORT || 3000
+
+const parseJson = (json) => {
+  const { From, Body } = json;
+
+  return {
+    phoneNumber: From,
+    message: Body.trim(),
+  };
+};
 
 app.post('/', (req, res) => {
-  res.send('Hello World!')
+  const { phoneNumber, message } = parseJson(req.body);
+  const cleanedMessage = message.trim().toLowerCase()
+
+  userModel.findByPhoneNumber(phoneNumber, (user) => {
+    const isSubscribeStep = stepsServices.isSubscribeStep(cleanedMessage, user)
+    const isUnsubscriptionStep = stepsServices.isUnsubscribeStep(cleanedMessage, user)
+    const isConfirmSubscriptionStep = stepsServices.isConfirmSubscriptionStep(cleanedMessage, user)
+    const isSetZipcodeStep = stepsServices.isSetZipcodeStep(cleanedMessage, user)
+    const isSetDistanceStep = stepsServices.isSetDistanceStep(cleanedMessage, user)
+
+    if (isSubscribeStep) {
+      userServices.initSubscription(phoneNumber)
+    } else if(isConfirmSubscriptionStep) {
+      userServices.requestZipcode(phoneNumber)
+    } else if(isUnsubscriptionStep) {
+      userServices.unsubscribe(phoneNumber)
+    } else if(isSetZipcodeStep) {
+      userServices.requestDistance(user, message)
+    } else if(isSetDistanceStep) {
+      userServices.triggerEvents(user, message)
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ status: 'ok' }));
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
