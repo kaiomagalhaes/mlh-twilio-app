67266c5c41e9f9f3886df9ff32a72d0a72dabe83
kaiomagalhaes <me@kaiomagalhaes.com>
2018-11-03 12:30:00 -0300

    add messages file

---
+++ messages.js
@@ -0,0 +1,21 @@
+const distance_to_events_message = "How far are you  willing to travel to events (in miles)"
+const enter_zipcode_message = 'Please enter your zipcode'
+const negative_message = 'no'
+const positive_message = 'yes'
+const receive_updates = 'Would you like to receive updates about MLH League Hackathons in your area?'
+const starting_message = 'mlh updates'
+const unsubscribe_message = "mlh unsubscribe"
+const unsubscribe_note_message = "Send 'mlh unsubscribe' at any moment to unsubscribe from our notifications."
+const unsubscribe_notification_message = "Thanks! You've been unsubscribed"
+
+module.exports = {
+  distance_to_events_message,
+  enter_zipcode_message,
+  negative_message,
+  positive_message,
+  receive_updates,
+  starting_message,
+  unsubscribe_message,
+  unsubscribe_note_message,
+  unsubscribe_notification_message,
+};
